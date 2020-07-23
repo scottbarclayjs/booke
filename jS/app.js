9f9ebc8b42be2f9563ce6c95f640322518cad88a
Scott Barclay <scottbarclay.js@gmail.com>
2020-07-22 22:54:14 -0700

    nav animation improvements

--- jS/app.js
+++ jS/app.js
@@ -3,8 +3,14 @@ let scrollLocation = 0;
 
 window.addEventListener('scroll', (e) => {
 	if (window.pageYOffset > 1) {
+		nav.classList.remove('unscroll');
 		nav.classList.add('scroll');
 	} else {
-		nav.classList.remove('scroll');
+		nav.classList.add('unscroll');
+	}
+	if (window.pageYOffset > 365) {
+		nav.classList.add('nav-min');
+	} else {
+		nav.classList.remove('nav-min');
 	}
 });
