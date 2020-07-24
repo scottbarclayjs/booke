b46e003a966eb784fece2bbdcbe4a09c28937126
Scott Barclay <scottbarclay.js@gmail.com>
2020-07-24 14:02:57 -0700

    added features and screenshot sections

--- jS/app.js
+++ jS/app.js
@@ -14,3 +14,48 @@ window.addEventListener('scroll', (e) => {
 		nav.classList.remove('nav-min');
 	}
 });
+
+const bookImages = document.querySelectorAll('.screenshot-image');
+const imageDot = document.querySelectorAll('.dot');
+let displayImage = 0;
+
+imageCycle();
+function imageCycle() {
+	for (let i = 0; i < bookImages.length; i++) {
+		bookImages[i].style.display = 'none';
+		imageDot[i].style.backgroundColor = '#aaa';
+	}
+	bookImages[displayImage].style.display = 'block';
+	imageDot[displayImage].style.backgroundColor = '#1c63fb';
+	if (displayImage < bookImages.length - 1) {
+		displayImage++;
+	} else {
+		displayImage = 0;
+	}
+}
+
+let t = setInterval(imageCycle, 4000);
+
+for (let i = 0; i < imageDot.length; i++) {
+	imageDot[i].addEventListener('click', () => {
+		displayImage = i;
+		imageCycle();
+	});
+}
+
+const imageNav = document.querySelectorAll('.screenshot-image-nav button');
+imageNav[0].addEventListener('click', () => {
+	if (displayImage > 1) {
+		displayImage -= 2;
+		imageCycle();
+	} else if (displayImage === 0) {
+		displayImage = bookImages.length - 2;
+		imageCycle();
+	} else {
+		displayImage = bookImages.length - 1;
+		imageCycle();
+	}
+});
+imageNav[1].addEventListener('click', () => {
+	imageCycle();
+});
